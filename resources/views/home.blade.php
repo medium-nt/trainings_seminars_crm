a212d5f9b403ee6a6dcd1319f8e9d3aae1e603da
skuze <skuzevanov@gmail.com>
2026-01-15 10:53:14 +0500

    start

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Главная страница</h5>
+        </div>
+    </div>
+@endsection
+
+@section('css')
+
+@stop
+
+@section('js')
+
+@stop
