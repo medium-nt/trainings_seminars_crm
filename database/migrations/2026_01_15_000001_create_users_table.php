cc29f8dc048f48c043c41e4b4c706253b138d402
skuze <skuzevanov@gmail.com>
2026-02-05 20:06:34 +0500

    Добавил возможность создавать юзеров с пустыми ФИО.

--- database/migrations/2026_01_15_000001_create_users_table.php
+++ database/migrations/2026_01_15_000001_create_users_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('last_name')->default('');
-            $table->string('name')->default('');
-            $table->string('patronymic')->default('');
+            $table->string('last_name')->nullable()->default('');
+            $table->string('name')->nullable()->default('');
+            $table->string('patronymic')->nullable()->default('');
             $table->string('email')->unique();
             $table->string('phone')->unique()->nullable();
             $table->unsignedBigInteger('role_id');
